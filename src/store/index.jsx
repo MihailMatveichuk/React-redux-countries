fdfb4b3c6e1b5d4fde72bc998ad8ba9f4f417974
Mikhail <mihkailmatveichuk@gmail.com>
2023-04-24 11:23:16 +0300

    feat: add switcher for theme

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,13 +1,13 @@
-import { compose, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import axios from 'axios';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
 import { rootReducer } from './root-reducer';
-import { createStoreHook } from 'react-redux';
 import * as api from '../config';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE && compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export const store = createStoreHook(
+const store = createStore(
 	rootReducer,
 	composeEnhancers(
 		applyMiddleware(
@@ -18,3 +18,5 @@ export const store = createStoreHook(
 		)
 	)
 );
+
+export { store };
