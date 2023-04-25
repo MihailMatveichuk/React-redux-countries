2b441103bddfd834d1f435092ad19774d8bea594
Mikhail <mihkailmatveichuk@gmail.com>
2023-04-25 20:54:34 +0300

    feat: add clearUp filters

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ import { useSelector, useDispatch } from 'react-redux';
 
 import { Container } from './Container';
 import { setTheme } from '../store/theme/theme-actions';
+import { clearControls } from '../store/controls/controls-actions';
 
 const HeaderEl = styled.header`
 	box-shadow: var(--shadow);
@@ -43,6 +44,10 @@ export const Header = () => {
 	const toggleTheme = () =>
 		dispatch(setTheme(theme === 'light' ? 'dark' : 'light'));
 
+	const cleanUp = () => {
+		dispatch(clearControls());
+	};
+
 	useEffect(() => {
 		document.body.setAttribute('data-theme', theme);
 	}, [theme]);
@@ -51,7 +56,7 @@ export const Header = () => {
 		<HeaderEl>
 			<Container>
 				<Wrapper>
-					<Title>Where is the world?</Title>
+					<Title onClick={cleanUp}>Where is the world?</Title>
 					<ModeSwitcher onClick={toggleTheme}>
 						{theme === 'light' ? (
 							<IoMoonOutline size='14px' />
