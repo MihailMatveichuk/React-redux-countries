56d77d9fb694946c967d2781bb7376c358ca2aac
Mikhail <mihkailmatveichuk@gmail.com>
2023-04-27 21:17:11 +0300

    feat: add countries page

--- src/store/root-reducer.jsx
+++ src/store/root-reducer.jsx
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { countriesReducer } from './countries/countries-reducer';
 import { themeReducer } from './theme/theme-reducer';
 import { controlsReducer } from './controls/controls-reducer';
+import { detailReducer } from './details/details-reducer';
 
 export const rootReducer = combineReducers({
 	theme: themeReducer,
 	countries: countriesReducer,
 	controls: controlsReducer,
+	details: detailReducer,
 });
